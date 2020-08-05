99279fbd2c6bca64f2e01d5055fd7902b02074dc
michellekim210 <69254574+michellekim210@users.noreply.github.com>
2020-08-05 11:33:31 -0400

    My first Xcode project

---
+++ StoryBoardIntro/StoryBoardIntro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StoryBoardIntro
+//
+//  Created by Michelle Kim on 8/5/20.
+//  Copyright © 2020 Michelle Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("This is a test")
+    }
+
+
+}
+
